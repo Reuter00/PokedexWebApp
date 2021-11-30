37b29e8d34565e9454119daa0f3e504b179d9c84
rreuter <ricardoreuter1@gmail.com>
2021-11-30 10:15:09 +0000

    Added: - Type dropdown
    To do:
    - fix sizes

--- pkmnlist/urls.py
+++ pkmnlist/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('generation', views.generations, name='generations'),  # Passing id and acessing it
     path('type', views.types, name='types'),  # Passing id and acessing it
     path('search', views.search, name='search')
-
 ]
